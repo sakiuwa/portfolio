ecc49e4acfaae3bf06880ac2a1ebca4e96e2f042
Saki Uwagawa <uwagawas@uw.edu>
2015-11-25 17:12:05 -0800

    Updategit status

--- js/main.js
+++ js/main.js
@@ -16,6 +16,36 @@ mainApp.config(function($stateProvider) {
 		templateUrl: 'templates/projects.html',
 		controller: 'projectsCtrl'
 	})
+	.state('prj1', {
+		url: '/projects/prj1',
+		templateUrl: 'templates/project/prj1.html',
+		controller: 'prj1Ctrl'
+	})
+	.state('prj2', {
+		url: '/projects/prj2',
+		templateUrl: 'templates/project/prj2.html',
+		controller: 'prj2Ctrl'
+	})
+	.state('prj3', {
+		url: '/projects/prj3',
+		templateUrl: 'templates/project/prj3.html',
+		controller: 'prj3Ctrl'
+	})
+	.state('prj4', {
+		url: '/projects/prj4',
+		templateUrl: 'templates/project/prj4.html',
+		controller: 'prj4Ctrl'
+	})
+	.state('prj5', {
+		url: '/projects/prj5',
+		templateUrl: 'templates/project/prj5.html',
+		controller: 'prj5Ctrl'
+	})
+	.state('prj6', {
+		url: '/projects/prj6',
+		templateUrl: 'templates/project/prj6.html',
+		controller: 'prj6Ctrl'
+	})
 });
 
 mainApp.controller('homeCtrl', function($scope) {
